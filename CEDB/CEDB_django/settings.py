cc7adaa38185adad9bc0e9aec857d66987cb32e7
Christopher Erb <christophererb01@gmail.com>
2023-08-02 15:49:30 -0400

    Removed slash

--- CEDB/CEDB_django/settings.py
+++ CEDB/CEDB_django/settings.py
@@ -59,8 +59,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5555",
     "http://127.0.0.1:5432",
-    "https://ced-db-production.up.railway.app/",
-    "https://famous-maamoul-915df2.netlify.app/"
+    "https://ced-db-production.up.railway.app",
+    "https://famous-maamoul-915df2.netlify.app"
     
 ]
 
