b556d594bdf0329052a8c69898d01428c2134e53
Christopher Erb <christophererb01@gmail.com>
2023-08-10 17:07:55 -0400

    test

--- CEDB/CEDB_django/settings.py
+++ CEDB/CEDB_django/settings.py
@@ -61,7 +61,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5432",
     "https://ced-db-production.up.railway.app",
     "https://famous-maamoul-915df2.netlify.app",
-    "https://drywall.chriserb.dev/"
+    "https://drywall.chriserb.dev"
     
 ]
 
